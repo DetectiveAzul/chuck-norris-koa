0cf0a555a50132a8cdf6869b21ca0d73e9c2387b
Jaime Lopez <detec.azul@gmail.com>
2018-08-23 14:49:44 +0100

    Removed JWT checkings from server.js

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
 const Koa = require('koa');
 const app = new Koa();
-const jwt = require('koa-jwt');
-const jwks = require('jwks-rsa');
 const cors = require('koa-cors');
 const bodyParser = require('koa-bodyparser');
 
 app.use(bodyParser());
 app.use(cors());
-
-//Auth0
-const authCheck = jwt(
-  {
-    secret: jwks.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: "https://devazul.eu.auth0.com/.well-known/jwks.json"
-    }),
-    audience: 'chucknorris.es',
-    issuer: "https://devazul.eu.auth0.com/",
-    algorithms: ['RS256']
-});
 
 //Routes
 const index = require('./router.js')
