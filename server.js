ab74fdd4862cb736579db14da8a134fc5352ef08
Jaime Lopez <detec.azul@gmail.com>
2018-08-23 09:50:11 +0100

    Changed server port from 3000 to 3001

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser());
 const index = require('./router.js')
 app.use(index.routes());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 //Server starts
 const server = app.listen(PORT, () => {
